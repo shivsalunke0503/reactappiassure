d8ae13c98bc3b1bcffa74bf58b6aefc5e203325e
shivsalunke0503 <shivsalunke.s@gmail.com>
2020-12-13 12:29:23 +0530

    Updates

---
+++ precache-manifest.631bb9af534b2502e036ef6de5bd4e2a.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64c151839b9e5aa76594b91ed12895d5",
+    "url": "/reactappiassure/index.html"
+  },
+  {
+    "revision": "0baf53fc2e09886067bf",
+    "url": "/reactappiassure/static/css/2.8439530f.chunk.css"
+  },
+  {
+    "revision": "a569a85e4cdb0f0e866f",
+    "url": "/reactappiassure/static/css/main.41aaeb4b.chunk.css"
+  },
+  {
+    "revision": "0baf53fc2e09886067bf",
+    "url": "/reactappiassure/static/js/2.0eb41659.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/reactappiassure/static/js/2.0eb41659.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a569a85e4cdb0f0e866f",
+    "url": "/reactappiassure/static/js/main.0e2cd9c1.chunk.js"
+  },
+  {
+    "revision": "74fba744ff47696994a7",
+    "url": "/reactappiassure/static/js/runtime-main.a0f9af8d.js"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/reactappiassure/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/reactappiassure/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/reactappiassure/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/reactappiassure/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/reactappiassure/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/reactappiassure/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/reactappiassure/static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/reactappiassure/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/reactappiassure/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/reactappiassure/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/reactappiassure/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/reactappiassure/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/reactappiassure/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/reactappiassure/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/reactappiassure/static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/reactappiassure/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/reactappiassure/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/reactappiassure/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/reactappiassure/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/reactappiassure/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
